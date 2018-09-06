1e73edb5994fed38d725a335ca533707d8b26410
LomoCode <lomocode@lomobook.local>
2018-09-07 11:38:30 +1200

    1. Guard transitionSuccessful result 2. Add currentViewController property

--- TaskTrackr/Paging/RootPagingViewController.swift
+++ TaskTrackr/Paging/RootPagingViewController.swift
@@ -11,37 +11,42 @@ import Parchment
 
 class RootPagingViewController: UIViewController, PagingViewControllerDelegate, PagingViewControllerDataSource {
     
+    var pagingViewController = PagingViewController<PagingIndexItem>()
+    var viewControllers:[UIViewController] = []
+    var currentViewController: UIViewController?
+    
     /*  PagingViewControllerDataSourceDataSource
      */
     func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, pagingItemForIndex index: Int) -> T where T : PagingItem, T : Comparable, T : Hashable {
-        return PagingIndexItem(index: index, title: viewControllers![index].title!) as! T
+        return PagingIndexItem(index: index, title: viewControllers[index].title!) as! T
     }
     
     
     func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, viewControllerForIndex index: Int) -> UIViewController where T : PagingItem, T : Comparable, T : Hashable {
-        return viewControllers![index]
+        return viewControllers[index]
     }
     
     
     func numberOfViewControllers<T>(in pagingViewController: PagingViewController<T>) -> Int where T : PagingItem, T : Comparable, T : Hashable {
-        return self.viewControllers!.count
+        return self.viewControllers.count
     }
     
     /* PagingViewControllerDelegate
      */
     func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, didScrollToItem pagingItem: T, startingViewController: UIViewController?, destinationViewController: UIViewController, transitionSuccessful: Bool) where T : PagingItem, T : Comparable, T : Hashable {
+        guard transitionSuccessful else {
+            return
+        }
         // update title to navigation bar
         updateNavigationBarTitle(to: destinationViewController.title!)
+        // update value of currentViewController
+        currentViewController = destinationViewController
     }
     
     func updateNavigationBarTitle(to newTitle: String) {
         let newTitle = String(format: "Manage %@", newTitle)
         self.navigationController?.navigationBar.topItem?.title = newTitle
     }
-    
-    var pagingViewController = PagingViewController<PagingIndexItem>()
-    
-    var viewControllers:[UIViewController]? = nil
     
     override func viewDidLoad() {
 
@@ -54,11 +59,10 @@ class RootPagingViewController: UIViewController, PagingViewControllerDelegate, 
                            getInstance(with: Constants.TOOL_PAGE),
                            getInstance(with: Constants.SITE_PAGE)]
         
-        guard viewControllers != nil else {return}
-        
         initPagingViewController()
         pagingViewController.delegate = self
         pagingViewController.dataSource = self
+        currentViewController = viewControllers[0]
         
         setLeftBarItem()
         
@@ -78,7 +82,7 @@ class RootPagingViewController: UIViewController, PagingViewControllerDelegate, 
         
     func getViewControllers() -> [UIViewController] {
         
-        return viewControllers!
+        return viewControllers
     }
     
     func setLeftBarItem() {
