078ed3b7fd23804a81829276be32725ec79e865f
LomoCode <lomocode@lomobook.local>
2018-09-14 11:25:14 +1200

    Deleted old segue "OpenProductForm"; Added a new segue with the same name; refer the segue to ProductModelsViewController

---
+++ TaskTrackr/Paging/ProductModelsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProductModelsViewController.swift
+//  TaskTrackr
+//
+//  Created by Eric Ho on 14/09/18.
+//  Copyright © 2018 LomoStudio. All rights reserved.
+//
+
+import UIKit
+
+class ProductModelsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
