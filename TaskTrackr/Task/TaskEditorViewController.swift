686572cecfff914dceb974aaca7c39aa6636c7c1
LomoCode <lomocode@Lomobook.local>
2018-10-08 22:34:34 +1300

    Added input and selector entries for TaskEditor

--- TaskTrackr/Task/TaskEditorViewController.swift
+++ TaskTrackr/Task/TaskEditorViewController.swift
@@ -30,9 +30,51 @@ class TaskEditorViewController: FormViewController {
     
     // build user input entry
     func buildEditor() {
+        
+        // Section Header
+        let createHeader : ((String) -> ViewFormer ) = { text in
+            return LabelViewFormer<FormLabelHeaderView>().configure {
+                $0.text = text
+                $0.viewHeight = 44
+            }
+        }
+        
+        // Menu
+        let createMenu : ((String, String, (() -> Void)?) -> RowFormer) = { (text, subText, onSelected) in
+            return LabelRowFormer<FormLabelCell>() {
+                $0.titleLabel.textColor = .formerColor()
+                $0.titleLabel.font = .boldSystemFont(ofSize: 16)
+                $0.accessoryType = .disclosureIndicator
+                }.configure {
+                    $0.text = text
+                    $0.subText = subText
+                }.onSelected({ _ in
+                    onSelected?()
+                })
+        }
+        
+        // Selector
+        let createSelectorRow = { (
+            text: String,
+            subText: String,
+            onSelected: ((RowFormer) -> Void)?
+            ) -> RowFormer in
+            return LabelRowFormer<FormLabelCell>() {
+                $0.titleLabel.textColor = .formerColor()
+                $0.titleLabel.font = .boldSystemFont(ofSize: 16)
+                $0.subTextLabel.textColor = .formerSubColor()
+                $0.subTextLabel.font = .boldSystemFont(ofSize: 14)
+                $0.accessoryType = .disclosureIndicator
+                }.configure { form in
+                    _ = onSelected.map { form.onSelected($0) }
+                    form.text = text
+                    form.subText = subText
+            }
+        }
+        
         // necessary elements: 1.title, 2.desc, 3.service, 4.designated workers, 5.due date, 6.location, 7.ref images
         // Enter Title
-        let nameField = TextFieldRowFormer<FormTextFieldCell>() {
+        let titleField = TextFieldRowFormer<FormTextFieldCell>() {
             $0.titleLabel.text = "Task Title"
             $0.titleLabel.font = .boldSystemFont(ofSize: 16)
             $0.textField.textColor = .formerSubColor()
@@ -56,15 +98,26 @@ class TaskEditorViewController: FormViewController {
                 // save Task desc
         }
         
-        // Select Service
+        // Select Service: Single Selection
+        let serviceSelector = createMenu("Select Service", "None Selected") { [weak self] in
+            // perform segue here:
+            //
+            } as? LabelRowFormer<FormLabelCell>
         
-        // Pickup Designated Workers
-        
+        // Pickup Designated Workers: Multi Selection
+        let workerSelector = createMenu("Designate Workers", "None Selected") { [weak self] in
+            // perform segue here:
+            //
+            } as? LabelRowFormer<FormLabelCell>
         // Select Due Date
         
         // Search&Pickup Location
         
         // Upload Images
+        
+        let sectionBasic = SectionFormer(rowFormer: titleField, descField).set(headerViewFormer: createHeader("Basic Task Info"))
+        let sectionSelectors = SectionFormer(rowFormer: serviceSelector!, workerSelector!).set(headerViewFormer: createHeader("Basic Task Info"))
+        former.append(sectionFormer: sectionBasic, sectionSelectors)
     }
 
     func saveNewTask() {
